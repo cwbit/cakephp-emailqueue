936e875e9bb0d3c32cec32f45fadcb0e7ce5f6e5
Chris Brescia <chris@cwbit.com>
2015-05-18 16:59:04 -0400

    all profile options now settable
    fixes #11

--- src/Controller/Component/EmailQueueComponent.php
+++ src/Controller/Component/EmailQueueComponent.php
@@ -9,6 +9,37 @@ use Cake\ORM\TableRegistry;
 use Cake\Utility\Hash;
 
 class EmailQueueComponent extends Component{
+
+    /**
+     * List of usable email variables
+     * 
+     * This is identical to Entity::$_accessible with the exception that there is no '*' (wildcard) allowed
+     * @var array
+     */
+    private $_accessible = [
+        'from' => true,         #: Email or array of sender. See Email::from().
+        'sender' => true,       #: Email or array of real sender. See Email::sender().
+        'to' => true,           #: Email or array of destination. See Email::to().
+        'cc' => true,           #: Email or array of carbon copy. See Email::cc().
+        'bcc' => true,          #: Email or array of blind carbon copy. See Email::bcc().
+        'replyTo' => true,      #: Email or array to reply the e-mail. See Email::replyTo().
+        'readReceipt' => true,  #: Email address or an array of addresses to receive the receipt of read. See Email::readReceipt().
+        'returnPath' => true,   #: Email address or and array of addresses to return if have some error. See Email::returnPath().
+        'messageId' => true,    #: Message ID of e-mail. See Email::messageId().
+        'subject' => true,      #: Subject of the message. See Email::subject().
+        'message' => false,     #: Content of message. Do not set this field if you are using rendered content.
+        'headers' => true,      #: Headers to be included. See Email::setHeaders().
+        'viewRender' => true,   #: If you are using rendered content, set the view classname. See Email::viewRender().
+        'template' => true,     #: If you are using rendered content, set the template name. See Email::template().
+        'theme' => true,        #: Theme used when rendering template. See Email::theme().
+        'layout' => true,       #: If you are using rendered content, set the layout to render. If you want to render a template without layout, set this field to null. See Email::template().
+        'viewVars' => true,     #: If you are using rendered content, set the array with variables to be used in the view. See Email::viewVars().
+        'attachments' => true,  #: List of files to attach. See Email::attachments().
+        'emailFormat' => true,  #: Format of email (html, text or both). See Email::emailFormat().
+        'transport' => true,    #: Transport configuration name. See Network\Email\Email::configTransport().
+        'log' => true,          #: Log level to log the email headers and message. true will use LOG_DEBUG. See also CakeLog::write()
+        'helpers' => true,      #: Array of helpers used in the email template.
+        ];
     
     /**
      * Queues and email for delivery by storing it in the 
@@ -37,6 +68,7 @@ class EmailQueueComponent extends Component{
     public function process(){
         $result = [];
 
+        # Get the EmailQueues table
         $this->EmailQueue = TableRegistry::get('EmailQueue.EmailQueues');
         
         # find all the emails we need to send
@@ -44,34 +76,34 @@ class EmailQueueComponent extends Component{
                             ->find('pending')
                             ->all();
 
+        # build and send each email
         foreach ($emails as $email):
+                # get the config settings for this email
+                $config = $this->_getConfig($email);
 
-            # get the config settings for this email
-            $config = $this->_getConfig($email);
+                # build and send the email
+                $e = new Email('default');
 
-            # build and send the email
-            $e = new Email('default');
-            $result[] = $e->template($config['template'], $config['layout'])
-                ->emailFormat($config['emailFormat'])
-                ->viewVars($config['viewVars'])
-                ->from($config['from'])
-                ->to($config['to_addr'])
-                ->cc($config['cc_addr'])
-                ->subject($config['subject'])
-                ->send();
+                # set the profile() by merging the $config'd settings with what we've deemed $_accessible (similar to Entity::$_accessible, ie. where [.., 'property' => true, ..])
+                $e->profile(array_intersect_key($config, array_filter($this->_accessible)));
 
-            # if we want to remove the email after it's sent, do so
-            if ($master['deleteAfterSend']):
-                $this->EmailQueue->delete($email);
+                # now that the email is built, send it
+                $result[] = $e->send();
 
-            # otherwise, just mark it sent and leave it there
-            else:
-                $email->status = 'sent';
-                $email->sent_on = date('Y-m-d H:i:s');
-                $this->EmailQueue->save($email);
-            endif;
+                # if we want to remove the email after it's sent, do so
+                if ($config['deleteAfterSend']):
+                    $this->EmailQueue->delete($email);
+
+                # otherwise, just mark it sent and leave it there
+                else:
+                    $email->status = 'sent';
+                    $email->sent_on = date('Y-m-d H:i:s');
+                    $this->EmailQueue->save($email);
+                endif;
+                
         endforeach;
 
+        # return the results from each email
         return $result;
     }
 
