20d5c1de40bcbb69b17510a19859cf3e78541292
Chris Brescia <chris@cwbit.com>
2016-06-10 15:01:41 -0400

    (fix) fixed namespacing

--- src/Processor/MarkdownProcessor.php
+++ src/Processor/MarkdownProcessor.php
@@ -3,8 +3,11 @@
 namespace EmailQueue\Processor;
 
 use EmailQueue\Processor\Processor;
-use Markdown\Lib\Markdown;
+use Markdown\Engine\Markdown;
 
+/**
+ * Add the MarkdownProcessor to the list of processors for your email to convert a markdown message body to HTML
+ */
 class MarkdownProcessor extends Processor
 {
     /**
@@ -12,6 +15,7 @@ class MarkdownProcessor extends Processor
      */
     public function process(array &$config)
     {
+      # run _message_html thru Markdown
       if (isset($config['viewVars']['_message_html'])) :
         $config['viewVars']['_message_html'] = Markdown::toHtml($config['viewVars']['_message_html']);
       endif;
--- src/Processor/MustacheProcessor.php
+++ src/Processor/MustacheProcessor.php
@@ -3,19 +3,26 @@
 namespace EmailQueue\Processor;
 
 use EmailQueue\Processor\Processor;
-use Mustache\Mustache_Engine as Mustache;
+use Mustache_Engine as Mustache;
 
-class MarkdownProcessor extends Processor
+/**
+ * Add the MustacheProcessor to your email's list of processors to parse the message bodies thru the Mustache Templateing Engine
+ * To see a list of supported styles, ..
+ * @see https://github.com/bobthecow/mustache.php/wiki
+ */
+class MustacheProcessor extends Processor
 {
     /**
      * Converts viewVars._message_html and viewVars._message_text
      */
     public function process(array &$config)
     {
-
+      # run _message_html thru Mustache
       if (isset($config['viewVars']['_message_html'])) :
         $config['viewVars']['_message_html'] = (new Mustache)->render($config['viewVars']['_message_html'], $config['viewVars']);
       endif;
+
+      # run _message_text thru Mustache
       if (isset($config['viewVars']['_message_text'])) :
         $config['viewVars']['_message_text'] = (new Mustache)->render($config['viewVars']['_message_text'], $config['viewVars']);
       endif;
